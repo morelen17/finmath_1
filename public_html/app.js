07c4d4534d99522c125874ed2424b64dd7ed8417
lenar <lenar-kazan@yandex.ru>
2016-11-27 00:53:36 +0300

    First commit

---
+++ public_html/app.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var N, S0, B0, a, b, p, q, r;
+
+function init() {
+    N = 0.0;
+    S0 = 0.0;
+    B0 = 0.0;
+    a = 0.0;
+    b = 0.0;
+    p = 0.0;
+    q = 0.0;
+    r = 0.0;
+}
+
+$(function() {
+    //alert ('jQuery подключен и отлично работает!');
+});
